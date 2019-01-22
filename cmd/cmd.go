b4dd1f3d99980c0f659f34f6a85d640152e44aa9
Vitalii Fedosieiev <fedoseevvt@nimses.com>
2019-01-22 10:45:18 +0200

    small update

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
 
 var config Config
+var l = logrus.New()
 
 func init() {
 	RootCmd.AddCommand(CMD)
@@ -13,6 +17,9 @@ var CMD = &cobra.Command{
 	Use:   "server",
 	Short: "run server",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		l.Info("start 12Climb server")
+		defer l.Info("stop 12Climb server")
+
 		return nil
 	},
 }
