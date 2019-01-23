5b7d2c0f2605c133e46b8a267754a9dc3d6f2d35
Vitalii Fedosieiev <fedoseevvt@nimses.com>
2019-01-23 16:47:51 +0200

    add correct test

--- internal/server/handler/handler_test.go
+++ internal/server/handler/handler_test.go
@@ -9,12 +9,14 @@ import (
 	"github.com/onetwoclimb/internal/server/restapi/operations"
 )
 
-func TestGetColors(t *testing.T) {
+func TestGetColorsCount(t *testing.T) {
 	h := New()
 	req := operations.BoardColorsParams{
 		HTTPRequest: &http.Request{},
 	}
 
 	r := h.GetColorsHandler(req)
-	assert.NotNil(t, r)
+	res := r.(*operations.BoardColorsOK)
+	assert.NotNil(t, res)
+	assert.Equal(t, 7, len(res.Payload.Colors))
 }
